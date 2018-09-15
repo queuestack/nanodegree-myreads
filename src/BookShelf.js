8c0ffd6b357df568f122f508d7e58d57ccc0d4bd
SeongkyuLee <seongkyu0617@gmail.com>
2018-09-15 22:33:55 +0900

    refactor: change stateful components to stateless components

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,34 +1,27 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Book from './Book'
 
-class BookShelf extends React.Component {
-    static propTypes = {
-        books: PropTypes.arrayOf(PropTypes.object),
-        bookshelfType: PropTypes.string.isRequired,
-        handleShelfChnage: PropTypes.func
-    }
+const BookShelf = (props) => {
+    const { books, bookshelfType, handleShelfChange } = props;
 
-    render() {
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.bookshelfType}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {this.props.books && this.props.books.map((book) => {
-                            return (
-                                <Book 
-                                    key={book.title} 
-                                    book={book} 
-                                    handleShelfChange={this.props.handleShelfChange}
-                                />
-                            )
-                        })}
-                    </ol>
-                </div>
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{bookshelfType}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books && books.map((book) => {
+                        return (
+                            <Book 
+                                key={book.title} 
+                                book={book} 
+                                handleShelfChange={handleShelfChange}
+                            />
+                        )
+                    })}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BookShelf;
--- src/Home.js
+++ src/Home.js
@@ -1,33 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { PropTypes } from 'prop-types'
 import BookList from './BookList'
 
-class Home extends React.Component {
-    static propTypes = {
-        books: PropTypes.object,
-        handleShelfChange: PropTypes.func
-    }
+const Home = (props) => {
+    const { books, handleShelfChange } = props;
 
-    render() {
-        const { books, handleShelfChange } = this.props;
-        console.log(books);
-
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                <h1>MyReads</h1>
-                </div>
-                <BookList 
-                    books={books}
-                    handleShelfChange={handleShelfChange}
-                />
-                <div className="open-search">
-                    <Link to="/search">Add a book</Link>
-                </div>
-            </div>       
-        ) 
-    }    
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+            <h1>MyReads</h1>
+            </div>
+            <BookList 
+                books={books}
+                handleShelfChange={handleShelfChange}
+            />
+            <div className="open-search">
+                <Link to="/search">Add a book</Link>
+            </div>
+        </div>   
+    )
 }
 
 export default Home;
